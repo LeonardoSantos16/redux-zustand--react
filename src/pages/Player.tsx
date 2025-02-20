f8280114e4a372be41dcb43cb32b06a5e6dae344
leonardo.mendes <leodsmf@gmail.com>
2025-02-20 20:05:55 -0300

    hook global

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -3,11 +3,19 @@ import { useAppSelector } from "../store";
 import { Header } from "../components/Header";
 import { Video } from "../components/Video";
 import { Module } from "../components/Module";
+import { useCurrentLesson } from "../store/slices/player";
+import { useEffect } from "react";
 
 export function Player() {
   const modules = useAppSelector(state => {
     return state.player.course.modules
   })
+
+  const { currentLesson } = useCurrentLesson()
+
+  useEffect(() => {
+    document.title = `Assistindo: ${currentLesson.title}`
+  }, [currentLesson]);
 
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
